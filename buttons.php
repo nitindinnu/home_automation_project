ae98bc2136def4071390e3b281a5ef6b7c9ee837
Nitin Dinnu <68476175+nitindinnu@users.noreply.github.com>
2022-01-10 14:34:16 +0530

    Add files via upload

---
+++ buttons.php
@@ -0,0 +1,166 @@
+<?php
+header( 'Location: http://13.233.25.184/projects/Home_Automation/main.html' ) ;
+
+$file = "buttonStatus.txt";
+$handle = fopen($file,'w+');
+if (isset($_POST['LON']))
+{
+$onstring = "LON";
+fwrite($handle,$onstring);
+fclose($handle);
+print "
+<html>
+<body>
+<title> KNRR </title>
+
+<style type=text/css>
+h1{
+        padding-left: 300px;
+}
+h2{
+        position: absolute;
+        top: 100px;
+        left: 450px;
+}
+</style>
+<h1>Internet of Things Implementation</h2>
+
+<h2> LIGHT ON </h2>
+</body>
+</html>
+";
+}
+
+else if(isset($_POST['LOFF']))
+{
+$offstring = "LOFF";
+fwrite($handle, $offstring);
+fclose($handle);
+print "
+<html>
+<body>
+<title> KNRR </title>
+<style type=text/css>
+h1{
+        padding-left: 300px;
+}
+h2{
+        position: absolute;
+        top: 100px;
+        left: 450px;
+}
+</style>
+<h1>Internet of Things Implementation</h2>
+<h2> LIGHT OFF </h2>
+</body>
+</html>
+";
+}
+
+else if(isset($_POST['BON']))
+{
+$offstring = "BON";
+fwrite($handle, $offstring);
+fclose($handle);
+print "
+<html>
+<body>
+<title> KNRR </title>
+<style type=text/css>
+h1{
+        padding-left: 300px;
+}
+h2{
+        position: absolute;
+        top: 100px;
+        left: 450px;
+}
+</style>
+<h1>Internet of Things Implementation</h2>
+<h2> BULB ON </h2>
+</body>
+</html>
+";
+}
+
+else if(isset($_POST['BOFF']))
+{
+$offstring = "BOFF";
+fwrite($handle, $offstring);
+fclose($handle);
+print "
+<html>
+<body>
+<title> KNRR </title>
+<style type=text/css>
+h1{
+        padding-left: 300px;
+}
+h2{
+        position: absolute;
+        top: 100px;
+        left: 450px;
+}
+</style>
+<h1>Internet of Things Implementation</h2>
+<h2> BULB OFF </h2>
+</body>
+</html>
+";
+}
+
+
+
+else if(isset($_POST['ALLON']))
+{
+$offstring = "ALLON";
+fwrite($handle, $offstring);
+fclose($handle);
+print "
+<html>
+<body>
+<title>KNRR </title>
+<style type=text/css>
+h1{
+        padding-left: 300px;
+}
+h2{
+        position: absolute;
+        top: 100px;
+        left: 450px;
+}
+</style>
+<h1>Internet of Things Implementation</h2>
+<h2> ALL DEVICES ON </h2>
+</body>
+</html>
+";
+}
+
+else if(isset($_POST['ALLOFF']))
+{
+$offstring = "ALLOFF";
+fwrite($handle, $offstring);
+fclose($handle);
+print "
+<html>
+<body>
+<title> KNRR </title>
+<style type=text/css>
+h1{
+        padding-left: 300px;
+}
+h2{
+        position: absolute;
+        top: 100px;
+        left: 450px;
+}
+</style>
+<h1>Internet of Things Implementation</h2>
+<h2> ALL DEVICES OFF </h2>
+</body>
+</html>
+";
+}
+
+?>
